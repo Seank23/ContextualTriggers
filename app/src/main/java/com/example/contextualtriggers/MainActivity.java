1fc1f4deece7e48dcae43b4e5417b20de14c1cd2
Dominic <dominic_colin@outlook.com>
2022-04-18 21:35:23 +0100

    Updated Permissions for Activity Recognition

--- app/src/main/java/com/example/contextualtriggers/MainActivity.java
+++ app/src/main/java/com/example/contextualtriggers/MainActivity.java
@@ -38,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
